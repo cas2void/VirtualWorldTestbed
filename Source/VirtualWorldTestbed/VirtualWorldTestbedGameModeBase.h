6d72ada792468eadc78bf3bdf83c22fa1934f0cb
cas2void <cas2void@gmail.com>
2022-05-10 23:23:05 +0800

    Add project

---
+++ Source/VirtualWorldTestbed/VirtualWorldTestbedGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VirtualWorldTestbedGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIRTUALWORLDTESTBED_API AVirtualWorldTestbedGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
